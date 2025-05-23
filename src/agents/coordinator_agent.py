f254f9e19397f0c3fe786ff1970d8021dc237d72
Adrián Hernández Castellanos <hadrianc02@gmail.com>
2025-05-23 15:54:54 -0400

    Iniciada la implementación de coordinator_agent

---
+++ src/agents/coordinator_agent.py
@@ -0,0 +1,8 @@
+class CoordinatorAgent:
+    def __init__(self):
+        # Acá más adelante se conectan los otros agentes
+        pass
+
+    def handle_query(self, user_input):
+        print(f"[Coordinador] Recibida la consulta: {user_input}")
+        # Aquí se decidirá qué agente manejará la petición
