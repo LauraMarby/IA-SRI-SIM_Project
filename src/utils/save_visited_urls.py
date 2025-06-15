2217086cb9e4b5913211b36373852354c5eba941
LauraMarby <lauramartir2@gmail.com>
2025-06-15 00:45:22 -0400

    funcion para añadir url nueva a las visitadas y verificar si ha sido visitada

--- src/utils/save_visited_urls.py
+++ src/utils/save_visited_urls.py
@@ -19,3 +19,19 @@ def save_data_visited_urls():
     with open(OUTPUT_FILE, "w") as f:
         f.write(contenido)
 
+def save_url_visited_urls(url: str):
+    """Añadir url nueva a la lista de urls visitadas."""
+    with open(OUTPUT_FILE, "r") as f:
+        data = f.read().strip()  
+
+    with open(OUTPUT_FILE, "a") as f:
+        if data:  
+            f.write(",")  
+        f.write(url) 
+
+def is_url_visited(url: str):
+    """Verificar si la url ha sido visitada."""
+    with open(OUTPUT_FILE, "r") as f:
+        data = f.read()
+        urls = data.split(",")  
+        return url in urls
